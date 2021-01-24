c2da7a92bde93e21ec3c25a547807611f127c09c
Elbie-em <emoonga1993@gmail.com>
2021-01-24 15:42:21 +0200

    Run database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_115135) do
+ActiveRecord::Schema.define(version: 2021_01_24_133326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,19 @@ ActiveRecord::Schema.define(version: 2021_01_23_115135) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.text "details"
+    t.string "architect_name"
+    t.integer "rating"
+    t.integer "price"
+    t.string "design_img_url"
+    t.string "blueprint_one_url"
+    t.string "blueprint_two_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
