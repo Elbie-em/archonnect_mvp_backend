9e58ded6ce8e94a80b760b7ecc1c479de7754e09
Elbie-em <emoonga1993@gmail.com>
2021-01-24 17:12:22 +0200

    Add resource for plan controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :registrations, only: [:create]
       delete :logout, to: "sessions#logout"
       get :logged_in, to: "sessions#logged_in"
+      resources :plans, only:[:index]
     end
   end
 end
