3b9d458a49ec3fe5d8f18462923e27a469a1dbeb
Elbie-em <emoonga1993@gmail.com>
2021-02-06 21:22:13 +0200

    Modify show method to search by email

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -14,8 +14,27 @@ module Api
             status: :created,
             user: user
           }
+        elsif !user
+          render json: {
+            status: 500,
+          }
+        end
+      end
+
+      def show
+        email = params[:email].to_s
+        @user = User.find_by(email: "#{email}.com")
+
+        if @user
+          render json: {
+            status: 202,
+            user: @user
+          }
         else
-          render json: {status: 500}
+          render json: {
+            status: 404,
+            user: []
+          }
         end
       end
     end
