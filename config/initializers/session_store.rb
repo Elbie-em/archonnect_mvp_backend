5fe84f48ee30a67db231ceef7fe9340ac2926602
Elbie-em <emoonga1993@gmail.com>
2021-02-10 12:43:14 +0200

    Change sessions store key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "archonnect_backend", domain: "api-archonnect-mvp.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp", domain: "api-archonnect-mvp.herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: "archonnect_backend"
 end
