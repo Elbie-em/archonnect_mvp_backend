7d094bbbe2beb8311335cb137819d1817990701a
Elbie-em <emoonga1993@gmail.com>
2021-02-10 12:45:44 +0200

    Change development domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
   Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp", domain: "api-archonnect-mvp.herokuapp.com"
-else
-  Rails.application.config.session_store :cookie_store, key: "archonnect_backend"
+elsif Rails.env == "development"
+  Rails.application.config.session_store :cookie_store, key: "archonnect_backend", domain: "api-archonnect-mvp.herokuapp.com"
 end
