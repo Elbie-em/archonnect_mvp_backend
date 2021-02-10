dbe6961a2acb3ed316dd1228eb074d5d0c753a73
Elbie-em <emoonga1993@gmail.com>
2021-02-11 01:54:04 +0200

    Change conditional format for production enviroment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-if Rails.env == "production"
+if Rails.env.production?
   Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp", domain: "api-archonnect-mvp.herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp"
