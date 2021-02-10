86c38cae1cfb171ff20e376d05432eabf5110233
Elbie-em <emoonga1993@gmail.com>
2021-02-11 00:48:29 +0200

    Allow all origins temp

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000","https://archonnect-mvp.herokuapp.com"
-    resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head],  credentials: true
+    origins "*"
+    resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head],  credentials: false
   end
 end
