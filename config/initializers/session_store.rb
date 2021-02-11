ea5e6f21fe8c9b18a5108c66ed10cdfe1457606c
Elbie-em <emoonga1993@gmail.com>
2021-02-11 02:01:33 +0200

    Modify session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,1 @@
-if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp", domain: "api-archonnect-mvp.herokuapp.com"
-else
-  Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp"
-end
+Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp"
