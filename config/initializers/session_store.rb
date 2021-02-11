f8fb8902f05b708b9f3aac7b524fd4ab478c7033
Elbie-em <emoonga1993@gmail.com>
2021-02-11 20:28:59 +0200

    Alter rails same site cookie params

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp", domain: "api-archonnect-mvp.herokuapp.com",same_site: :none, secure: true
+  Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp", domain: "api-archonnect-mvp.herokuapp.com",same_site: :none, secure: false
 else
   Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp"
 end
