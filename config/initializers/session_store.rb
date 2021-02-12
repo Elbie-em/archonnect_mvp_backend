549ec6ea14f0f416665560d24d72c836d650248f
Elbie-em <emoonga1993@gmail.com>
2021-02-12 16:07:19 +0200

    milestone-three/complete-deploy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp', same_site: :none, secure: true
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp', same_site: :none, secure: true
+else
+  Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp'
+end
