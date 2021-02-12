c41b59909efb711dcaf37989c8c5bb143dcacf9f
Elbie-em <emoonga1993@gmail.com>
2021-02-13 01:37:00 +0200

    Reinitialize session store same site

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp', domain: :all
+Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp', domain: :all, same_site: :none, secure: true
 
