b083087756bbb01256bdd21aa268e51f67ad9992
Elbie-em <emoonga1993@gmail.com>
2021-02-12 23:19:35 +0200

    Add new api domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp'
+Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp', domain: 'http://142.93.62.189'
 
