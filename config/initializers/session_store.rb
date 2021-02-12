88177af57fb1bcbcbfe709eaa4be4fbb4465eedc
Elbie-em <emoonga1993@gmail.com>
2021-02-12 23:07:55 +0200

    Temp removal of same site

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,2 @@
-if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp', same_site: :none, secure: true
-else
-  Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp'
-end
+Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp'
+
