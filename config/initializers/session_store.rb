4095822d8ed140679caed433b34e1203ba47febd
Elbie-em <emoonga1993@gmail.com>
2021-02-22 18:41:28 +0200

    Restore session store to default

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: 'api-archonnect-mvp', domain: :all
-
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp", domain: "https://api-archonnect-mvp.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "api-archonnect-mvp"
+end
