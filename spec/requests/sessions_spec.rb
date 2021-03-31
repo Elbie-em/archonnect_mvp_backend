990790204071119eb484175898cd551d97065435
Elbie-em <emoonga1993@gmail.com>
2021-03-31 20:20:17 +0200

    Uncomment test in sessions controller

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,22 +1,23 @@
 require 'rails_helper'
 
-# describe 'Check if user is logged in', type: :request do
-#   before do
-#     post '/api/v1/registrations', params: {
-#       user: {
-#         email: 'johndoe@gmail.com',
-#         password: 'asdfasdf',
-#         password_confirmation: 'asdfasdf'}
-#     }
+describe 'Check if user is logged in', type: :request do
+  before do
+    post '/api/v1/registrations', params: {
+      user: {
+        email: 'johndoe@gmail.com',
+        password: 'asdfasdf',
+        password_confirmation: 'asdfasdf'
+      }
+    }
 
-#     get '/api/v1/logged_in'
-#   end
+    get '/api/v1/logged_in'
+  end
 
-#   it 'return user with logged in status as true' do
-#     data = JSON.parse(response.body)
-#     expect(data["logged_in"]).to eq(true)
-#   end
-# end
+  it 'return user with logged in status as true' do
+    data = JSON.parse(response.body)
+    expect(data['logged_in']).to eq(true)
+  end
+end
 
 describe 'Check if user is logged out', type: :request do
   before do
